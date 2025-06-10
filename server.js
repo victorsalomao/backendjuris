229b092f31ae82f63c8d013763ab559a88febe02
Victor Salomão <victor.salomaor@gmail.com>
2025-06-10 01:20:03 -0300

    ajustes no server para o path correto do deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const analyzeRouter = require("routes/analyze");
+const analyzeRouter = require("./routes/analyze");
 
 dotenv.config();
 
